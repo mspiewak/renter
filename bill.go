68c8c5e9a040e54da735160d0d370943fd99170f
Marcin Spiewak <mspiewak85@gmail.com>
2018-03-19 23:19:45 +0000

    updated json struct

--- bill.go
+++ bill.go
@@ -8,25 +8,35 @@ import (
 
 // BillType keeps information about bill type, e.x. electricity, gas
 type BillType struct {
-	ID   int
-	Name string
+	ID   int    `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
+}
+
+type NestedBillType struct {
+	BillType
+	ID int `json:"id" db:"bill_type_id"`
 }
 
 // Bill keeps information about particular bill
 type Bill struct {
-	ID          int       `json:"id" db:"id"`
-	Price       float32   `json:"price" db:"price"`
-	DueDate     time.Time `json:"due_date" db:"due_date"`
-	PeriodStart time.Time `json:"period_start" db:"period_start"`
-	PeriodEnd   time.Time `json:"period_end" db:"period_end"`
-	URL         *string   `json:"url" db:"url"`
-	BillTypeID  int       `json:"bill_type_id" db:"bill_type_id"`
+	NestedBillType `json:"type"`
+	ID             int       `json:"id" db:"id"`
+	Price          float32   `json:"price" db:"price"`
+	DueDate        time.Time `json:"due_date" db:"due_date"`
+	PeriodStart    time.Time `json:"period_start" db:"period_start"`
+	PeriodEnd      time.Time `json:"period_end" db:"period_end"`
+	URL            *string   `json:"url" db:"url"`
+}
+
+type NestedBill struct {
+	Bill
+	ID int `json:"id" db:"bill_id"`
 }
 
 // TenantBill keeps information about bill breakdown per particular tenant
 type TenantBill struct {
+	NestedBill  `json:"bill"`
 	ID          int        `json:"id" db:"id"`
-	BillID      int        `json:"bill_id" db:"bill_id"`
 	TenantID    int        `json:"tenant_id" db:"tenant_id"`
 	Price       float32    `json:"price" db:"price"`
 	PaymentDate *time.Time `json:"payment_date" db:"payment_date"`
@@ -40,6 +50,12 @@ func getBills(db *sqlx.DB) ([]Bill, error) {
 
 func getTenantBills(db *sqlx.DB, id int) ([]TenantBill, error) {
 	var bills []TenantBill
-	err := db.Select(&bills, "SELECT * FROM tenant_bill WHERE tenant_id = ?", id)
+	err := db.Select(&bills, `
+		SELECT *
+		FROM tenant_bill tb 
+		LEFT JOIN bill b ON (b.id=tb.bill_id) 
+		LEFT JOIN bill_type bt ON (b.bill_type_id=bt.id) 
+		WHERE tenant_id = ?
+		`, id)
 	return bills, err
 }
