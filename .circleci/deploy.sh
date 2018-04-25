941139247f0b9a9f42650dfc6b3a05184e075f9e
Marcin Spiewak <mspiewak85@gmail.com>
2018-04-25 12:46:20 +0100

    test

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -55,39 +55,39 @@ echo "Updated task def revision: $UPDATED_TASK_DEF_REVISION"
 echo "switch over to the new task definition by selecting the newest revision"
 SUCCESS_UPDATE=$(aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_NAME --cluster $CLUSTER_NAME)
 
-echo "Verify the new task definition attached and the old task definitions de-register aka cleanup"
-for attempt in {1..8}; do
-    #will return true if the updated task def is fully up and running in the service and the primary task def
-    IS_ECS_READY=$(aws ecs describe-services --cluster $CLUSTER_NAME --services $SERVICE_NAME | jq '.services[0] .deployments | .[] | select(.taskDefinition == '${UPDATED_TASK_DEF_REVISION}') | (.desiredCount == .runningCount and .status == "PRIMARY")')
-    echo "Is ECS updated: $IS_ECS_READY"
-    if [ $IS_ECS_READY = false ]; then
-        echo "Waiting for $UPDATED_TASK_DEF_REVISION"
-        echo "It needs to become the primary task def and reach desired instance count"
-        sleep 20
-        echo "Lets find all active task definitions"
-        ACTIVE_TASK_DEFS=$(aws ecs list-task-definitions --family-prefix $TASK_NAME | jq '.taskDefinitionArns')
-        echo "Here are the active task definitions: $ACTIVE_TASK_DEFS"
-        #will return true if there are more than 1 task definitions still active
-        IS_MULTIPLE_ACTIVE_TASK_DEFS=$(echo $ACTIVE_TASK_DEFS | jq 'map(select(. != '${UPDATED_TASK_DEF_REVISION}')) | length > 1')
-        echo "Are there multiple active ones: $IS_MULTIPLE_ACTIVE_TASK_DEFS"
-        IS_ALL_TASKS_DRAINED=false #should default this to false, so it doesnt throw an error below
-        continue
-    elif [ $IS_ALL_TASKS_DRAINED = true ]; then
-        echo "Successfully cleaned up old tasks and running the new task."
-        PRIMARY_TASK=$(aws ecs list-task-definitions --family-prefix $TASK_NAME | jq '.taskDefinitionArns[]')
-        echo "$PRIMARY_TASK is the only running task"
-        break
-    else
-        #iterate through the active tasks and register them
-        echo $ACTIVE_TASK_DEFS | jq -r '.[] | select(. != '${UPDATED_TASK_DEF_REVISION}')' | \
-        while read arn; do
-            deregistered_status=$(aws ecs deregister-task-definition --task-definition $arn | jq '.taskDefinition .status');
-            echo "Setting $arn task definition to " + $(echo $deregistered_status)
-        done
-        ACTIVE_TASK_DEFS=$(aws ecs list-task-definitions --family-prefix $TASK_NAME | jq '.taskDefinitionArns')
-        echo "All obsolete tasks have been moved to INACTIVE"
-        echo "but we want to make sure they are drained as well"
-        sleep 30
-        IS_ALL_TASKS_DRAINED=$(aws ecs describe-services --cluster $CLUSTER_NAME --services $SERVICE_NAME | jq '.services[0] .deployments | length == 1')
-        echo "Are all obsolete tasks drained/stopped: $IS_ALL_TASKS_DRAINED"
-    fi
+# echo "Verify the new task definition attached and the old task definitions de-register aka cleanup"
+# for attempt in {1..8}; do
+#     #will return true if the updated task def is fully up and running in the service and the primary task def
+#     IS_ECS_READY=$(aws ecs describe-services --cluster $CLUSTER_NAME --services $SERVICE_NAME | jq '.services[0] .deployments | .[] | select(.taskDefinition == '${UPDATED_TASK_DEF_REVISION}') | (.desiredCount == .runningCount and .status == "PRIMARY")')
+#     echo "Is ECS updated: $IS_ECS_READY"
+#     if [ $IS_ECS_READY = false ]; then
+#         echo "Waiting for $UPDATED_TASK_DEF_REVISION"
+#         echo "It needs to become the primary task def and reach desired instance count"
+#         sleep 20
+#         echo "Lets find all active task definitions"
+#         ACTIVE_TASK_DEFS=$(aws ecs list-task-definitions --family-prefix $TASK_NAME | jq '.taskDefinitionArns')
+#         echo "Here are the active task definitions: $ACTIVE_TASK_DEFS"
+#         #will return true if there are more than 1 task definitions still active
+#         IS_MULTIPLE_ACTIVE_TASK_DEFS=$(echo $ACTIVE_TASK_DEFS | jq 'map(select(. != '${UPDATED_TASK_DEF_REVISION}')) | length > 1')
+#         echo "Are there multiple active ones: $IS_MULTIPLE_ACTIVE_TASK_DEFS"
+#         IS_ALL_TASKS_DRAINED=false #should default this to false, so it doesnt throw an error below
+#         continue
+#     elif [ $IS_ALL_TASKS_DRAINED = true ]; then
+#         echo "Successfully cleaned up old tasks and running the new task."
+#         PRIMARY_TASK=$(aws ecs list-task-definitions --family-prefix $TASK_NAME | jq '.taskDefinitionArns[]')
+#         echo "$PRIMARY_TASK is the only running task"
+#         break
+#     else
+#         #iterate through the active tasks and register them
+#         echo $ACTIVE_TASK_DEFS | jq -r '.[] | select(. != '${UPDATED_TASK_DEF_REVISION}')' | \
+#         while read arn; do
+#             deregistered_status=$(aws ecs deregister-task-definition --task-definition $arn | jq '.taskDefinition .status');
+#             echo "Setting $arn task definition to " + $(echo $deregistered_status)
+#         done
+#         ACTIVE_TASK_DEFS=$(aws ecs list-task-definitions --family-prefix $TASK_NAME | jq '.taskDefinitionArns')
+#         echo "All obsolete tasks have been moved to INACTIVE"
+#         echo "but we want to make sure they are drained as well"
+#         sleep 30
+#         IS_ALL_TASKS_DRAINED=$(aws ecs describe-services --cluster $CLUSTER_NAME --services $SERVICE_NAME | jq '.services[0] .deployments | length == 1')
+#         echo "Are all obsolete tasks drained/stopped: $IS_ALL_TASKS_DRAINED"
+#     fi
