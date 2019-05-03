e08cc6ce9816afe6a56ba5dede8d26d215b3a4e2
Marcin Spiewak <marcin.spiewak@8x8.com>
2019-05-03 14:14:18 +0200

    updated db

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 
 // Initialize the application
 func (a *App) Initialize() error {
-	dbc, err := sqlx.Connect("mysql", "root:EKZ9DZNNnphQ@tcp(db:3306)/renter?parseTime=true")
+	dbc, err := sqlx.Connect("mysql", "root:Jg2FXug3rg@tcp(db:3306)/renter?parseTime=true")
 	if err != nil {
 		return fmt.Errorf("cannot connect to db server: %v", err)
 	}
